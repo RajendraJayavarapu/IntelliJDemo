17924aa9bb1c4a842f14b483d51cfe4fa0fff450
Rajendra.Jayavarapu <rajendra.4045@gmail.com>
2020-03-03 14:43:21 +0530

    Added display method to Main.java

--- src/org/tavant/Main.java
+++ src/org/tavant/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        display();
+    }
+
+    private static void display(){
+        System.out.println("Display my message!");
     }
 }
